8a4d6862fb0acc9342bcfd0865e5c6d44af88e2e
jmgonja <jj.mgonja@gmail.com>
2021-09-28 14:29:56 +0300

    First Commit

---
+++ components/Button.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Button = ({ variant }) => {
+  return <button className={`btn ${variant}`}>Book Now</button>;
+};
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { Button } from '../components/Button';
+import { Heading } from '../components/Heading';
+
+export default function Home() {
+  return (
+    <div>
+      <Heading />
+      <Button variant="btn-secondary" />
+    </div>
+  );
+}
