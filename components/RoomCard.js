ecdca90f7726724422fe048064858c0c2d1aad48
muddy700 <mohamedmfaume700@gmail.com>
2021-10-12 21:23:59 +0300

    Finalized Issa's Job

--- components/RoomCard.js
+++ components/RoomCard.js
@@ -6,7 +6,7 @@ import Hotel from '../public/hotel.jpg';
 const RoomCard = ({ roomTitle }) => {
   return (
     <Card className="bg-dark text-white">
-      <Image src={Hotel} alt="Card image" width={60} height={250} />
+      <Image src={Hotel} alt="Card image" width={300} height={250} />
       <Card.ImgOverlay>
         <div className="room-price">
           <span>$85</span> Book Now
